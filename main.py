a619bfd23d32758bd4dfad118f41eaa3c7475848
bgute12 <124756682+bgute12@users.noreply.github.com>
2025-10-31 16:56:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.uix.boxlayout import BoxLayout
 # Holes defined with pos_hint (relative coordinates inside the green)
 HOLES = [
     {"id": 1, "pos_hint": (0.0874, 0.6573), "radius": 7, "last_points": None},
-    {"id": 2, "pos_hint": (0.20, 0.299), "radius": 7, "last_points": None},
+    {"id": 2, "pos_hint": (0.3724, 0.8498), "radius": 7, "last_points": None},
     {"id": 3, "pos_hint": (0.25, 0.50), "radius": 7, "last_points": None},
     {"id": 4, "pos_hint": (0.3125, 0.4420), "radius": 7, "last_points": None},
     {"id": 5, "pos_hint": (0.9144, 0.3375), "radius": 7, "last_points": None},
